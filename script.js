d24aabd0d645ac1a7c62be3a7022fca540fe4d44
Deepam Wagh <deepam.wagh@gmail.com>
2025-11-23 21:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const priceCells = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  priceCells.forEach((cell) => {
+    total += Number(cell.textContent);
+  });
+
+  const table = document.querySelector("table");
+
+  const newRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+
+  totalCell.setAttribute("colspan", "2");
+
+  totalCell.textContent = `Total Price = Rs ${total}`;
+  newRow.appendChild(totalCell);
+
+  table.appendChild(newRow);
+
+  getSumBtn.disabled = true;
 };
 
 getSumBtn.addEventListener("click", getSum);
